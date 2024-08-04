0e00de76226e1fa2861af8ca31eeffe652c1bf1a
prasad-25 <83207070+prasad-25@users.noreply.github.com>
2024-08-04 18:23:50 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 //your first program
 //
 class Hello {
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		system.out.println("Welcome to Gits online Training!");
 	}
 }
